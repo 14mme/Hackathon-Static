e3e8729caab8867547d6ade3a239a3d513723d9d
14mme <fvck1nhum4n@gmail.com>
2024-10-10 07:40:40 +0700

    commit

---
+++ assets/home.js
@@ -0,0 +1,35 @@
+const username = localStorage.getItem("username");
+
+if (username) {
+    document.getElementById("homepage").innerText = `Halo, ${username}!`;
+}
+
+window.onload = function() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    const authButton = document.getElementById('authButton');
+    const mAuthButton = document.getElementById('mAuthButton');
+
+    if (isLoggedIn) {
+        authButton.innerText = 'Keluar';
+        authButton.href = '#'; 
+        authButton.onclick = function() {
+            localStorage.removeItem('isLoggedIn');
+            localStorage.removeItem('username');
+            location.reload(); 
+        };
+
+        mAuthButton.innerText = 'Keluar';
+        mAuthButton.href = '#';
+        mAuthButton.onclick = function() {
+            localStorage.removeItem('isLoggedIn');
+            localStorage.removeItem('username');
+            location.reload(); 
+        };
+    } else {
+        authButton.innerText = 'Masuk';
+        authButton.href = '/pages/Login.html';
+
+        mAuthButton.innerText = 'Masuk';
+        mAuthButton.href = '/pages/Login.html';
+    }
+};
---
+++ assets/script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    localStorage.setItem('isLoggedIn', 'true');
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    localStorage.setItem("username", username);
+
+    window.location.href = "../index.html";
+});
+
+
